ea61afad56342145e11bb23884ecd6e6b3d284be
Mafka <kitm@digt.ru>
2023-05-06 17:35:12 +0300

    free-Miles

---
+++ src/freeMiles.java
@@ -0,0 +1,12 @@
+public class freeMiles {
+    public static void main(String[] args) {
+
+        int ticketPrice = 200;
+        int numberRublesOneMile = 20;
+
+        int freeMiles = (ticketPrice / numberRublesOneMile);
+
+        System.out.println(freeMiles);
+
+    }
+}
